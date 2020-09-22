f8f074b364825d7a2dda6e2e2d8c8e7be926007e
Mohameth Seck <mohamethseck34@gmail.com>
2020-09-22 17:29:32 -0400

    added nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {useState ,useEffect} from 'react'
 import "./Nav.css";
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow (true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img 
                 className="nav__logo"
-                src = "https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=21"
+                src = "https://www.freepnglogos.com/uploads/netflix-logo-0.png"
                 alt="Netflix Logo"
             />
 
             <img 
-                className="nav__logo"
-                src = "https://miro.medium.com/fit/c/256/256/1*7ysB83lEAkeoS4d9Dg0g5A.jpeg"
+                className="nav__avatar"
+                src = "https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
                 alt="Netflix Logo"
             />
         </div>
