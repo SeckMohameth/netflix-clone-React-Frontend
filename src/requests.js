6cb9b0a7266e6292452cd6e32843971e53884355
Mohameth Seck <mohamethseck34@gmail.com>
2020-09-22 13:56:08 -0400

    needed changes

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "0200b0029dc1414ce39bfc3273044dc2";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
